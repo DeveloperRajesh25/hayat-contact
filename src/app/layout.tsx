4f424167d754fb725f3454fb9e2e7d33cafe3bbb
DeveloperRajesh25 <talaganarajesh@gmail.com>
2026-04-23 16:14:01 +0530

    tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,37 +32,23 @@ export default function RootLayout({
       <body className="antialiased">
         {/* Google Tag Manager (noscript) */}
         <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TSPBG2TZ"
-            height="0" 
-            width="0" 
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KNW9F9GG"
+            height="0"
+            width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-        
+
         {/* Google Tag Manager Script */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TSPBG2TZ');`}
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KNW9F9GG');`}
         </Script>
 
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-F0PH6XBJH2"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-F0PH6XBJH2');
-          `}
-        </Script>
         {children}
       </body>
     </html>
